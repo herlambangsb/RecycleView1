f7dbb3674cb18bb86e36529b603c85b8483978a5
Yessy Permatasari <yessypermatasari08@gmail.com>
2016-10-30 17:33:33 +0700

    Initial Commit RecyclerView List

---
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recycleview1/Hotel.java
@@ -0,0 +1,19 @@
+package id.sch.smktelkom_mlg.learn.recycleview1;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by Yess on 30/10/2016.
+ */
+
+public class Hotel {
+    public String judul;
+    public String deskripsi;
+    public Drawable foto;
+
+    public Hotel(String judul, String deskripsi, Drawable foto) {
+        this.judul = judul;
+        this.deskripsi = deskripsi;
+        this.foto = foto;
+    }
+}
